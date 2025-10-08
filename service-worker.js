dd17bd994ae118e82a3d870376da8ec2fe2c1ee8
esaave <114452059+esaave@users.noreply.github.com>
2025-10-08 03:53:45 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,46 @@
-const CACHE_NAME = 'orange-store-cache-v1';
+const CACHE_NAME = 'orange-store-v2';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/accesorios.html',
-  '/repuestos.html',
-  '/login.html',
-  '/logo.png',
-  '/logopeq.png',
-  '/fondo1.jpg',
-  '/fondo2.jpg',
-  '/fondo3.jpg',
-  '/fondo4.jpg',
-  '/precios.json',
-  '/repuestos.json'
+  './',
+  './index.html',
+  './accesorios.html',
+  './repuestos.html',
+  './login.html',
+  './logo.png',
+  './logopeq.png',
+  './fondo1.jpg',
+  './fondo2.jpg',
+  './fondo3.jpg',
+  './fondo4.jpg',
+  './precios.json',
+  './repuestos.json',
+  './manifest.json'
 ];
 
-// Instalar Service Worker y cachear recursos
-self.addEventListener('install', (event) => {
+// Instalar y cachear
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-// Servir desde cache o red
-self.addEventListener('fetch', (event) => {
+// Activar y limpiar viejos caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+});
+
+// Interceptar peticiones
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+      .catch(() => caches.match('./index.html'))
   );
 });
