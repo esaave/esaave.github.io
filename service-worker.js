7ed7d6a33d9ffb7877be7dec650eff7da556ae6c
esaave <114452059+esaave@users.noreply.github.com>
2025-10-08 04:12:55 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,48 @@
-const CACHE_NAME = 'orange-store-v2';
+const CACHE_NAME = 'orange-store-v3';
 const urlsToCache = [
-  './',
-  './index.html',
-  './accesorios.html',
-  './repuestos.html',
-  './login.html',
-  './logo.png',
-  './logopeq.png',
-  './fondo1.jpg',
-  './fondo2.jpg',
-  './fondo3.jpg',
-  './fondo4.jpg',
-  './precios.json',
-  './repuestos.json',
-  './manifest.json'
+  '/orangestore/',
+  '/orangestore/index.html',
+  '/orangestore/login.html',
+  '/orangestore/accesorios.html',
+  '/orangestore/repuestos.html',
+  '/orangestore/logo.png',
+  '/orangestore/logopeq.png',
+  '/orangestore/fondo1.jpg',
+  '/orangestore/fondo2.jpg',
+  '/orangestore/fondo3.jpg',
+  '/orangestore/fondo4.jpg',
+  '/orangestore/precios.json'
 ];
 
-// Instalar y cachear
+// Instalar y cachear recursos
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
-// Activar y limpiar viejos caches
+// Activar y eliminar versiones viejas
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      );
-    })
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+// Responder desde caché o red
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request).then(res => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, res.clone());
+          return res;
+        });
+      }).catch(() => caches.match('/orangestore/index.html'))
+    )
   );
 });
 
-// Interceptar peticiones
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-      .catch(() => caches.match('./index.html'))
-  );
-});
