4220cc76b7b4cc21896e0f81cf987914aa58cc47
esaave <114452059+esaave@users.noreply.github.com>
2025-10-08 06:22:37 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // ======================================================
-// 🌐 Orange Store PWA - Service Worker (versión v3.2.2)
+// 🌐 Orange Store PWA - Service Worker (versión v3.2.3)
 // ======================================================
 
-const CACHE_NAME = 'orange-store-v3.2.2';
+const CACHE_NAME = 'orange-store-v3.2.3';
 const urlsToCache = [
   '/',                     
   '/index.html',           
