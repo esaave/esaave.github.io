d1e9518b829d8f5013b6f7b306c7f66ab07782ff
esaave <114452059+esaave@users.noreply.github.com>
2025-11-07 06:44:42 -0400

    Update service worker version to v3.4.6

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // ======================================================
-// 🌐 Orange Store PWA - Service Worker (versión v3.4.5)
+// 🌐 Orange Store PWA - Service Worker (versión v3.4.6)
 // ======================================================
 
-const CACHE_NAME = 'orange-store-v3.4.5';
+const CACHE_NAME = 'orange-store-v3.4.6';
 const urlsToCache = [
   '/',                     
   '/index.html',           
